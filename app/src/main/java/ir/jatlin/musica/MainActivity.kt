73128ffb83212ee3b96ef39af2517c46edfc41db
MENasri23 <m.e26159487@gmail.com>
2022-05-10 19:12:00 -0700

    Annotate MainActivity

--- app/src/main/java/ir/jatlin/musica/MainActivity.kt
+++ app/src/main/java/ir/jatlin/musica/MainActivity.kt
@@ -10,8 +10,16 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import dagger.hilt.android.AndroidEntryPoint
 import ir.jatlin.musica.ui.theme.MusicaTheme
+import ir.jatlin.musica.ui.theme.songs.Songs
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,7 +30,18 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    val navController = rememberNavController()
+
+                    NavHost(
+                        navController = navController,
+                        startDestination = "songs"
+                    ) {
+
+                        composable("songs") {
+                            Songs(songsViewModel = hiltViewModel())
+                        }
+
+                    }
                 }
             }
         }
