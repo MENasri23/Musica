d70f496d46e34ca4680b84bee36c6f06ec4353f6
MENasri23 <m.e26159487@gmail.com>
2022-05-13 06:42:00 -0700

    Enable timber debug mode

--- app/src/main/java/ir/jatlin/musica/MusicaApplication.kt
+++ app/src/main/java/ir/jatlin/musica/MusicaApplication.kt
@@ -2,7 +2,15 @@ package ir.jatlin.musica
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 @HiltAndroidApp
 class MusicaApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+    }
 }
